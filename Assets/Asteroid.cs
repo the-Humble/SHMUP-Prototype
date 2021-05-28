d572b8ed310c7799b02ccd3383b630e0d528ef57
Gabriel Zamora <pg20gabriel@vfs.com>
2021-05-27 23:12:02 -0500

    Asteroid moving and spawning v0.1

---
+++ Assets/Asteroid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Limit")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(/*TODO: Place projectile differentiator here*/ true)
+        {
+            // TODO: Check game state for enemy destroyed by projectile
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/AsteroidSpawner.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] GameObject asteroid;
+    [SerializeField] float spawnCooldown;
+    [SerializeField] float offsetSpawner;
+
+    private float spawnTimer;
+    private float spawnHeight;
+    private float lastSpawnHeight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnTimer <= 0)
+        {
+            HeightRandomizer();
+
+            GameObject.Instantiate(asteroid,
+                new Vector3(spawnPoint.position.x, 
+                spawnPoint.position.y + spawnHeight, 
+                spawnPoint.position.z),
+                Quaternion.identity);
+
+            spawnTimer = spawnCooldown;
+        }
+
+        spawnTimer -= Time.deltaTime;
+    }
+
+    private void HeightRandomizer()
+    {
+        while (true)
+        {
+            spawnHeight = Random.Range(-4f, 4f);
+
+            if (CheckNoSpawnLocationRepeat())
+            {
+                return;
+            }
+        }
+    }
+
+    private bool CheckNoSpawnLocationRepeat()
+    {
+        if (lastSpawnHeight <= (spawnHeight - offsetSpawner) 
+            || lastSpawnHeight >= (spawnHeight + offsetSpawner))
+        {
+            lastSpawnHeight = spawnHeight;
+            return true;
+        }
+
+        return false;
+    }
+}
