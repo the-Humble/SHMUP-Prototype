7e55eb623d26f20dd65b0170d79c4f7f78e04527
Ben Spencer <pg20ben@vfs.com>
2021-06-01 13:58:18 -0700

    Started On Player Lives

---
+++ Assets/Scripts/LifeBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LifeBar : MonoBehaviour
+{
+    private GameManager manager;
+    [SerializeField] private TextMeshProUGUI text;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (manager)
+            text.text = "x" + manager.playerLives;
+        else Debug.Log("no Manager");
+    }
+}
