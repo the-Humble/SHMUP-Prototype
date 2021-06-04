f2669201730837e79477c50ff802c9a2a3bdd373
Gabriel Zamora <pg20gabriel@vfs.com>
2021-06-04 17:14:41 -0500

    Asteroids spawn 3 mini-asteroids while destroyed, destroy 40 asteroids to win (400 score without timer), timer reduced to 250, new asteroid sprites added

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    [SerializeField] private int scoreValue = 10;
-    [SerializeField] private PowerUp[] drops;
-    [Range(0,1)][SerializeField] private float PUPDropChance = .5f;
-    [SerializeField] private ParticleSystem deathPaticles;
+    [SerializeField] protected int scoreValue = 10;
+    [SerializeField] protected PowerUp[] drops;
+    [SerializeField] private GameObject miniAsteroid;
+    [Range(0,1)][SerializeField] protected float PUPDropChance = .5f;
+    [SerializeField] protected ParticleSystem deathPaticles;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    protected void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Limit")
         {
@@ -17,20 +18,18 @@ public class Asteroid : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    protected virtual void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.GetComponent<Projectile>() != null)
         {
-            // TODO: Check game state for enemy destroyed by projectile
             // Add to score
             Score.AddScore(scoreValue);
 
-
-            if ((float)Random.Range(0, 10) /10 <= PUPDropChance){
+            /*if ((float)Random.Range(0, 10) /10 <= PUPDropChance){
                 var PUP = drops[Random.Range(0, drops.Length)];
                 if (PUP)
                     Instantiate(PUP, transform.position, Quaternion.identity);
-            }
+            }*/
 
             FindObjectOfType<GameManager>().enemieskilled++;
 
@@ -39,6 +38,10 @@ public class Asteroid : MonoBehaviour
                 var temp = Instantiate(deathPaticles, transform.position, Quaternion.identity);
                 Destroy(temp, 2);
             }
+
+            Instantiate(miniAsteroid, transform.position, Quaternion.identity);
+            Instantiate(miniAsteroid, transform.position, Quaternion.identity);
+            Instantiate(miniAsteroid, transform.position, Quaternion.identity);
 
             Destroy(this.gameObject);
         }
--- Assets/Scripts/AsteroidMove.cs
+++ Assets/Scripts/AsteroidMove.cs
@@ -7,9 +7,6 @@ public class AsteroidMove : MonoBehaviour
     // Speed of the asteroid's movement
     [SerializeField] private float moveSpeed = 3f;
 
-    // Time to elapse in order to change direction
-    [SerializeField] private float moveTime = 1f;
-
     // Horizontal direction
     [Range(-1, 0)]
     [SerializeField] private float directionX = 1f;
@@ -21,53 +18,20 @@ public class AsteroidMove : MonoBehaviour
     // assign starting direction to move
     protected Vector3 direction;
 
-    // bool change direction flag
-    protected bool oppositeDir = false;
-
     private void Awake()
     {
+        float randomPos = Random.Range(-1, 2);
+        float randomSpeed = Random.Range(0, 3);
+
+        moveSpeed += randomSpeed;
+
         // Initialize direction vector
-        direction = new Vector3(directionX, directionY, 0);
+        direction = new Vector3(directionX, randomPos, 0);
 
-        // Start IEnumerator Coroutine
-        StartCoroutine(isMoving());
     }
 
-    private IEnumerator isMoving()
+    private void Start()
     {
-        // Run always
-        while (true)
-        {
-            // Check opposite direction flag to mirror the direction
-            if (oppositeDir)
-            {
-                // mirror direction
-                direction = new Vector3(directionX, -directionY, 0); ;
-            }
-
-            // start move coroutine passing the actual direction
-            yield return StartCoroutine(MoveCoroutine(direction));
-
-            // toggle flags
-            oppositeDir = !oppositeDir;
-        }
-    }
-
-    private IEnumerator MoveCoroutine(Vector3 moveDir)
-    {
-        // Elapsed time 
-        float duration = 0;
-
-        // check if elapsed time hasn't reached the move time
-        while (duration < moveTime)
-        {
-            // increase elapsed time
-            duration += Time.deltaTime;
-
-            // update position
-            transform.position += ((moveDir * Time.deltaTime) * moveSpeed);
-
-            yield return null;
-        }
+        this.gameObject.GetComponent<Rigidbody2D>().velocity = direction.normalized * moveSpeed;
     }
 }
